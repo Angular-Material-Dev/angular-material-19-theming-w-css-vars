65063328ebc2b26ac77e3edd284f5455326924a5
Dharmen Shah <shhdharmen@gmail.com>
2024-12-24 12:18:39 +0530

    feat: complete example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavigationComponent,
+    DashboardComponent,
+    AddressFormComponent,
+    TableComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-material-19-theming';
